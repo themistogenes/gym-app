1af9da38f842008e514922a5a30e2fa15fbccfd1
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-09-05 16:08:20 -0400

    create components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import Hero from "./components/Hero"
+import Generator from "./components/Generator"
+import Workout from "./components/Workout"
+
 function App() {
 
   return (
     <>
-
+      <Hero />
+      <Generator />
+      <Workout />
     </>
   )
 }
