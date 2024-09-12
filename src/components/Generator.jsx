9fb57befcdd7278e7ffd1adaa738a17f79e9f066
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-09-11 20:02:03 -0400

    schemes section

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import SectionWrapper from "./SectionWrapper"
-import { WORKOUTS } from "../utils/swoldier"
+import { WORKOUTS, SCHEMES } from "../utils/swoldier"
 
 function Header({ index, title, description }) {
   return (
@@ -34,7 +34,7 @@ export default function Generator() {
         title={'Pick your poison.'}
         description={'Select the workout you wish to endure.'}
       />
-      <div className="grid grid-cols-2 sm:grid-cols-4 gap-4">
+      <div className="grid grid-cols-3 sm:grid-cols-4 gap-4">
         {
           Object.keys(WORKOUTS).map((type, typeIndex) => {
             return (
@@ -67,6 +67,28 @@ export default function Generator() {
           showModal && (
             <div>modal</div>
           )
+        }
+      </div>
+      <Header
+        index={'03'}
+        title={'Become juggernaut.'}
+        description={'Select your ultimate objective.'}
+      />
+      <div className="grid grid-cols-2 gap-4">
+        {
+          Object.keys(SCHEMES).map((scheme, schemeIndex) => {
+            return (
+              <button 
+                className="bg-slate-950 border border-blue-400 duration-200 hover:border-blue-600 py-3 rounded-lg" 
+                key={schemeIndex}
+                onClick={() => {}}
+              >
+                <p className="capitalize">
+                  {scheme.replaceAll('_', ' ')}
+                </p>
+              </button>
+            )
+          })
         }
       </div>
     </SectionWrapper>
