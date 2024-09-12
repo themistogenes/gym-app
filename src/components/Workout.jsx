b9d3257492d66004dedbb0fc5c41a76fa905cf4e
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-09-12 11:22:12 -0400

    Workout component

--- src/components/Workout.jsx
+++ src/components/Workout.jsx
@@ -1,5 +1,20 @@
+import SectionWrapper from "./SectionWrapper"
+
 export default function Workout({ workout }) {
   return (
-    <div>Workout</div>
+    <SectionWrapper 
+    header={"Welcome to"} 
+    title={['The', 'DANGER', 'Zone']}
+    >
+      <div className="flex flex-col gap-4">
+        {
+          workout.map((exercise, i) => {
+            return (
+              'workout'
+            )
+          })
+        }
+      </div>
+    </SectionWrapper>
   )
 }
